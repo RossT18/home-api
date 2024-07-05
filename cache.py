e3ba10bdfc951607df5ae4e10b4b03111df966d1
Ross Tinsley <rtinsley1199@gmail.com>
2024-07-06 00:28:49 +0100

    Add caching for server data

---
+++ cache.py
@@ -0,0 +1,64 @@
+import json
+import os
+import shutil
+import datetime
+
+class CachedValue():
+
+  def __init__(self, filename: str, folder: str = "data"):
+    self.filename = filename
+    self._filepath = f'{folder}/{filename}.json'
+    self._internal = {}
+
+    if os.path.exists(self._filepath):
+      self._update_from_disk()
+    else:
+      # Create file if it doesn't exist
+      os.makedirs(os.path.dirname(self._filepath), exist_ok=True)
+      # Write initial value to the file to create it
+      self._store_to_disk()
+
+  def _store_to_disk(self):
+    with open(self._filepath, "w") as f:
+      f.write(json.dumps(self._internal, indent=2))
+
+  def _update_from_disk(self):
+    try:
+      with open(self._filepath, "r") as df:
+        self._internal = json.load(df)
+    except:
+      # Error loading existing file, overwriting with initial value...
+      # But store a backup first
+      now_str = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d-%H-%M-%S")
+      backup_path = f'backups/{now_str}/{self._filepath}'
+      os.makedirs(os.path.dirname(backup_path), exist_ok=True)
+      shutil.copy2(self._filepath, backup_path)
+      self._store_to_disk()
+
+  def read(self):
+    return self._internal
+  
+  def save(self, action) -> bool:
+    try:
+      self._internal = action(self._internal)
+      self._store_to_disk()
+      return True
+    except:
+      print('[Cache Err] An exception occurred when updating internal value')
+
+    return False
+  
+## To deal with diffs
+# When updating server, timestamp it
+# Then when a device reconnects to the server
+# and has modifications with a more recent timestamp
+# and the server hasn't had modifications since 
+# the device was disconnected
+# then we can update the server with the cache directly
+
+# if the server has received modifications since the
+# device was last connected, and it has no changes
+# update the device
+
+# if both server and device have had modifications.
+# allow user to perform the diff
--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from pydantic.utils import deep_update
+
+from cache import CachedValue
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class TodoItem(BaseModel):
+  task: str
+  completed: bool
+  timestamp: str
 
+todos = CachedValue('todos')
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/todos")
+def get_todos():
+    return todos.read()
+
+@app.put('/add/{todo_id}', response_model=TodoItem)
+def add_todo(todo_id: str, new_todo: TodoItem):
+  
+  def saver(existing):
+    # raise Exception('intentional bug')
+    return deep_update(existing, jsonable_encoder({ todo_id: new_todo }))
+    # return jsonable_encoder({ todo_id: new_todo })
+  
+  todos.save(saver)
+
+  return new_todo
