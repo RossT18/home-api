1868e3c5f39bffdfafc4fd881835afb404e2116e
Ross Tinsley <rtinsley1199@gmail.com>
2024-10-24 23:55:21 +0100

    Update weather API provider

--- routes/weather.py
+++ routes/weather.py
@@ -17,18 +17,79 @@ router = APIRouter(
 class Weather(BaseModel):
   sunrise: str
   sunset: str
-  temp: int
+  temperature: int
   weather: str
-  description: str
   icon: str
 
+class WeatherType:
+  name: str
+  icon: str
+
+def get_weather_type_from_code(code: int) -> WeatherType:
+  def get_icon_url(icon_name):
+    valid_icons = ['01d', '02d', '03d', '04d', '09d', '10d', '11d', '13d', '50d']
+    if icon_name not in valid_icons:
+      # Return clear sky by default
+      print(f'Error getting weather icon with name {icon_name}')
+      return "http://openweathermap.org/img/wn/01d@2x.png"
+    return f"http://openweathermap.org/img/wn/{icon_name}@2x.png"
+
+  codes = {
+    0: { 'name': 'Clear sky', 'icon': get_icon_url('01d') },
+    1: { 'name': 'Mainly clear', 'icon': get_icon_url('01d') },
+    2: { 'name': 'Partly cloudy', 'icon': get_icon_url('02d') },
+    3: { 'name': 'Overcast', 'icon': get_icon_url('03d') },
+    45: { 'name': 'Fog', 'icon': get_icon_url('50d') },
+    48: { 'name': 'Fog', 'icon': get_icon_url('50d') },
+    51: { 'name': 'Light drizzle', 'icon': get_icon_url('10d') },
+    53: { 'name': 'Drizzle', 'icon': get_icon_url('10d') },
+    55: { 'name': 'Heavy drizzle', 'icon': get_icon_url('10d') },
+    56: { 'name': 'Light freezing drizzle', 'icon': get_icon_url('10d') },
+    57: { 'name': 'Heavy freezing drizzle', 'icon': get_icon_url('10d') },
+    61: { 'name': 'Light rain', 'icon': get_icon_url('10d') },
+    63: { 'name': 'Rain', 'icon': get_icon_url('10d') },
+    65: { 'name': 'Heavy rain', 'icon': get_icon_url('09d') },
+    66: { 'name': 'Light freezing rain', 'icon': get_icon_url('13d') },
+    67: { 'name': 'Heavy freezing rain', 'icon': get_icon_url('13d') },
+    71: { 'name': 'Light snow', 'icon': get_icon_url('13d') },
+    73: { 'name': 'Snow', 'icon': get_icon_url('13d') },
+    75: { 'name': 'Heavy snow', 'icon': get_icon_url('13d') },
+    77: { 'name': 'Snow grains', 'icon': get_icon_url('13d') },
+    80: { 'name': 'Light rain showers', 'icon': get_icon_url('09d') },
+    81: { 'name': 'Rain showers', 'icon': get_icon_url('09d') },
+    82: { 'name': 'Heavy rain showers', 'icon': get_icon_url('09d') },
+    85: { 'name': 'Light snow showers', 'icon': get_icon_url('13d') },
+    86: { 'name': 'Heavy snow showers', 'icon': get_icon_url('13d') },
+    95: { 'name': 'Thunderstorms', 'icon': get_icon_url('11d') },
+    96: { 'name': 'Thunderstorms and hail', 'icon': get_icon_url('11d') },
+    99: { 'name': 'Thunderstorms and heavy hail', 'icon': get_icon_url('11d') }
+  }
+  return codes[code] if code in codes else { 'name': 'Weather Unavailable', 'icon': get_icon_url('') }
+
 def get_weather_response():
-  api_key = os.getenv('OPENWEATHERMAP_API_KEY')
   lat = os.getenv('LAT')
   lon = os.getenv('LON')
-  if api_key is None or lat is None or lon is None:
-    raise HTTPException(status_code=500, detail=f'Secret weather data could not be loaded ({"API_KEY" if api_key is None else "LOCATION"})')
-  url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+  if lat is None or lon is None:
+    raise HTTPException(status_code=500, detail=f'Secret weather data (LOCATION) could not be loaded')
+
+  open_meteo_api_params = {
+    "latitude": str(lat),
+    "longitude": str(lon),
+    "current": "temperature_2m,apparent_temperature,precipitation,weather_code",
+    "hourly": "temperature_2m,apparent_temperature,precipitation,weather_code",
+    "daily": "temperature_2m_max,temperature_2m_min,sunrise,sunset",
+    "wind_speed_unit": "mph",
+    "timezone": "Europe%2FLondon",
+    "past_days": "1",
+    "forecast_days": "3",
+    "timeformat": "unixtime",
+    "models": "ukmo_seamless"
+  }
+  url = 'https://api.open-meteo.com/v1/forecast?'
+
+  for key, value in open_meteo_api_params.items():
+    url += f"{key}={value}&"
+
   headers = {'Content-type': 'application/json'}
   r = requests.get(url, headers=headers)
   if r.status_code != 200:
@@ -37,15 +98,16 @@ def get_weather_response():
 
 def format_weather_response(data) -> Weather:
   def format_time(timestamp):
-    return datetime.fromtimestamp(timestamp, ZoneInfo('Europe/London')).strftime("%H:%M")
+    return datetime.fromtimestamp(timestamp, ZoneInfo(data['timezone'])).strftime("%H:%M")
+  
   current = data['current']
+  weather_type = get_weather_type_from_code(current['weather_code'])
   return Weather(
-    sunrise=format_time(current['sunrise']),
-    sunset=format_time(current['sunset']),
-    temp=int(round(current['temp'])),
-    weather=current['weather'][0]['main'],
-    description=current['weather'][0]['description'],
-    icon=current['weather'][0]['icon']
+    sunrise=format_time(data['daily']['sunrise'][1]),
+    sunset=format_time(data['daily']['sunset'][1]),
+    temperature=int(round(current['temperature_2m'])),
+    weather=weather_type['name'],
+    icon=weather_type['icon']
   )
 
 @router.get('/', response_model=Weather)
