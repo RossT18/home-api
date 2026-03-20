389e42057e9e1f5a34d4a550d05bfb6547e8a989
Ross Tinsley <rtinsley1199@gmail.com>
2026-03-20 18:24:24 +0000

    refactor(bins): Swap colours for bin names and improve formatting logic

--- app/services/bin_collection/__init__.py
+++ app/services/bin_collection/__init__.py
@@ -1,10 +1,9 @@
 from .models import BinSchedule, Collection
-from .main import get_next_bin, format_bin_schedule_response, get_bin_url_response
+from .main import format_bin_schedule_response, get_bin_url_response
 
 __all__ = [
 	"BinSchedule",
 	"Collection",
-	"get_next_bin",
 	"format_bin_schedule_response",
 	"get_bin_url_response",
 ]
